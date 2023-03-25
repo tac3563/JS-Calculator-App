9e199aeb78a1fdef178e7319921b19dfaa5aa6f0
tac3563 <tcox_bu@outlook.com>
2023-03-25 11:58:43 +0000

    [Update] inital commit and projects setup

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+    Calculator App:
+
+    1. Three buttons 'start' 'stop' 'reset'.
+    2. A stopwatch in which the digits increase over time.
+        2a.
+    3. On click, start button should start the timer.
+    4. On click, stop button should pause the timer.
+    5. On click, reset button should reset the timer back to zero.
+*/
